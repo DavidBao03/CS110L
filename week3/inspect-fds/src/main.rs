c998f2608dabc23932e51ac6f5a6ddb9a8e48f29
DavidBao003 <1353406961@qq.com>
2024-03-06 20:07:40 +0800

    Part1: milestone 1 first on MacOS

--- week3/inspect-fds/src/main.rs
+++ week3/inspect-fds/src/main.rs
@@ -10,11 +10,19 @@ fn main() {
         println!("Usage: {} <name or pid of target>", args[0]);
         std::process::exit(1);
     }
-    #[allow(unused)] // TODO: delete this line for Milestone 1
+    //#[allow(unused)] // TODO: delete this line for Milestone 1
     let target = &args[1];
 
     // TODO: Milestone 1: Get the target Process using psutils::get_target()
-    unimplemented!();
+    let process = match ps_utils::get_target(target).expect("Fail to get Process!") {
+        Some(process) => process,
+        None      => {
+                println!("Fail to get PID!");
+                std::process::exit(1);
+        }
+    };
+
+    println!("find pid: {}", process.pid);
 }
 
 #[cfg(test)]
